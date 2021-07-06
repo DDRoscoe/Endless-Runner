6d41fc687dbce9798a506c36348ba635e6198507
Jalen Pastor <jalenpastor@gmail.com>
2021-07-05 17:23:00 -0700

    merged all changes + cloud enemy test

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -20,7 +20,7 @@ class Play extends Phaser.Scene {
     // place tile sprite
     // this.background = this.add.tileSprite(0, 0, 360, 640, 'background').setOrigin(0,0);
     this.background = this.add.tileSprite(0, 0, 360, 640, 'field').setOrigin(0,0);
-    this.cloudies = this.add.tileSprite(0, 0, 360, 640, 'cloudsalone').setOrigin(0,0);
+    this.cloudies = this.add.tileSprite(0, 0, 360, 640, 'overcast').setOrigin(0,0);
 
     //place bird
     this.player = new Bird(this, game.config.width/2, game.config.height - UISize - 45, 'bird').setOrigin(0.5, 0);
@@ -76,6 +76,8 @@ class Play extends Phaser.Scene {
 
     if (!this.gameOver) {
       this.background.tilePositionY -= 1;
+      this.cloudies.tilePositionY -= 1;
+      this.cloudies.tilePositionX -= 0.5;
       this.player.update();
       this.enemy1.update();      
       this.score++;
